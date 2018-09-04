0521cd9188f640afb0217f7abe2f41999f679622
Ilya <31817995+Makasinov@users.noreply.github.com>
2018-09-04 08:05:37 +0300

    Программа может уже проходить бета тест. Также можно проверять картинки в большем размере, в данном случае 25х25px. Чтобы поднять разрешение нужно менять исходный код, позже исправлю и можно будет из под параметров это делать

--- main.cpp
+++ main.cpp
@@ -4,25 +4,43 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <iostream>
+#include <string>
 
-using namespace std;
 using namespace cv;
+
+const char * filename {"C:\\Files\\test.jpg"}; // FILENAME CHANGE HERE (PATH)
 
 struct Point
 {
-    int x = 0;
-    int y = 0;
+    struct {
+        int x_u = 0, x_l = 0; // x upper cross & x lower cross
+        int y = 0;            // y mid cross
+        char x_uInfo = ' ';   // l - left, r - right, b - both
+        char x_lInfo = ' ';
+    } cross;
+    int x = 0;                // x cord left upper angle
+    int y = 0;                // y cord left upper angle
     int wide = 0;
     int height = 0;
+
+    void info()
+    {
+        printf("\nx: %d, y: %d, wide: %d, height: %d",x,y,wide,height);
+        printf("\ncross.y: %d, cross.x_u: %d, cross.x_l: %d",cross.y,cross.x_u,cross.x_l);
+        printf("\ncross.x_uInfo: %c, cross.x_lInfo: %c\n",cross.x_uInfo,cross.x_lInfo);
+    }
 };
 
-const int COLS_MAX_SIZE = 15;
-const int ROWS_MAX_SIZE = 15;
-const char * filename {"C:\\Files\\three#2.jpg"};
+
+
+const int COLS_MAX_SIZE = 25;   // Matrix columns size
+const int ROWS_MAX_SIZE = 25;   // Matrix rows size
 
 bool arr[COLS_MAX_SIZE][ROWS_MAX_SIZE];
 
 ::Point getAttr(bool arr[COLS_MAX_SIZE][ROWS_MAX_SIZE]);
+::Point getCross(bool arr[COLS_MAX_SIZE][ROWS_MAX_SIZE], ::Point point);
+int getResult(bool arr[COLS_MAX_SIZE][ROWS_MAX_SIZE], ::Point p);
 
 void drawArr(bool arr[COLS_MAX_SIZE][ROWS_MAX_SIZE])
 {
@@ -30,9 +48,9 @@ void drawArr(bool arr[COLS_MAX_SIZE][ROWS_MAX_SIZE])
     {
         for(int y = 0; y < COLS_MAX_SIZE; y++)
         {
-            cout << arr[x][y] << " ";
+            std::cout << arr[x][y] << " ";
         }
-        cout << endl;
+        std::cout << std::endl;
     }
 }
 
@@ -46,15 +64,15 @@ void drawArr(bool arr[COLS_MAX_SIZE][ROWS_MAX_SIZE])
         if (myExit) break;
         for(int x = 0; x < ROWS_MAX_SIZE; x++)
         {
-            printf("arr[%d][%d]",x+1,y+1);
+            //printf("arr[%d][%d]",x+1,y+1);
             if (arr[x][y] == 1)
             {
-                printf(" <--\n");
+                //printf(" <--\n");
                 tmp.x = x;
                 tmp.y = y;
                 myExit = true;
             }
-            else printf("\n");
+            //else printf("\n");
             if (myExit) break;
         }
     }
@@ -63,25 +81,23 @@ void drawArr(bool arr[COLS_MAX_SIZE][ROWS_MAX_SIZE])
     for(int x = 0; x < ROWS_MAX_SIZE; x++)
     for(int y = tmp.y; y < COLS_MAX_SIZE; y++)
     {
-        printf("arr[%d][%d]",x+1,y+1);
+        //printf("arr[%d][%d]",x+1,y+1);
         if (arr[x][y] == 1)
         {
-            printf(" <--\n");
+            //printf(" <--\n");
             tmp.x = tmp.y + 1;
             tmp.y = x+1;
             return tmp;
         }
-        else printf("\n");
+        //else printf("\n");
     }
+    return tmp;
 }
 
 ::Point getAttr(bool arr[COLS_MAX_SIZE][ROWS_MAX_SIZE])
 {
-    cout << endl;
+    std::cout << std::endl;
     ::Point point,tmp,tmp2;
-    bool isBlack;
-    if (arr[0][0] == 0) isBlack = true;
-        else isBlack = false;
 
     bool foundedFirstWhiteColor = false;
 
@@ -129,16 +145,124 @@ void drawArr(bool arr[COLS_MAX_SIZE][ROWS_MAX_SIZE])
     point.x = tmp.x;
     point.y = tmp.y;
 
-    cout << "Point is: x - " << point.x << ", y - " << point.y <<
-            ",\n wide - " << point.wide << ", height - " << point.height<< endl;
+    //std::cout << "Point is: x - " << point.x << ", y - " << point.y << ", wide - " << point.wide << ", height - " << point.height << std::endl;
 
     return point;
 }
 
+::Point getCross(bool arr[COLS_MAX_SIZE][ROWS_MAX_SIZE], ::Point p)
+{
+    int pos = 0;
+    int crossedTimes = 0;
+    bool isCrossed = false;
+    std::cout << std::endl;
+    //Get y
+    for(int i = 0; i < COLS_MAX_SIZE; i++ )
+    {
+        pos = (p.x - 1) + (p.wide / 2);
+        //std::cout << "    " << arr[i][pos] << std::endl;      // DEBUG
+        if (!isCrossed && arr[i][pos] == 1)
+        {
+            isCrossed = true;
+            crossedTimes++;
+        }
+        if (isCrossed && arr[i][pos] == 0)
+            isCrossed = false;
+    }
+    p.cross.y = crossedTimes;
+    crossedTimes = 0;
+    isCrossed = false;
+    std::cout << std::endl;
+    // Get x_u
+    for(int i = 0; i < ROWS_MAX_SIZE; i++ )
+    {
+        pos = (p.y - 1) + (p.height / 4);
+        //std::cout << arr[pos][i];             // DEBUG
+        if (!isCrossed && arr[pos][i] == 1)
+        {
+            isCrossed = true;
+            crossedTimes++;
+            if (i <= COLS_MAX_SIZE / 2)
+                p.cross.x_uInfo = 'l';
+            else
+            {
+                if (p.cross.x_uInfo == 'l')
+                    p.cross.x_uInfo = 'b';
+                else p.cross.x_uInfo = 'r';
+            }
+        }
+        if (isCrossed && arr[pos][i] == 0)
+            isCrossed = false;
+    }
+    p.cross.x_u = crossedTimes;
+    crossedTimes = 0;
+    isCrossed = false;
+    //std::cout << std::endl;
+    // Get x_l
+    for(int i = 0; i < ROWS_MAX_SIZE; i++ )
+    {
+        pos = (p.y - 1) + (p.height / 1.5);
+        //std::cout << arr[pos][i];             // DEBUG
+        if (!isCrossed && arr[pos][i] == 1)
+        {
+            isCrossed = true;
+            crossedTimes++;
+            if (i <= COLS_MAX_SIZE / 2)
+                p.cross.x_lInfo = 'l';
+            else
+            {
+                if (p.cross.x_lInfo == 'l')
+                    p.cross.x_lInfo = 'b';
+                else p.cross.x_lInfo = 'r';
+            }
+        }
+        if (isCrossed && arr[pos][i] == 0)
+            isCrossed = false;
+    }
+    p.cross.x_l = crossedTimes;
+
+    return p;
+}
+
+int getResult(bool arr[COLS_MAX_SIZE][ROWS_MAX_SIZE], ::Point p)
+{
+    if (p.wide >= COLS_MAX_SIZE || p.height >= ROWS_MAX_SIZE)
+        return -1;
+    if (p.cross.y == 2 && p.cross.x_l == 2 && p.cross.x_u == 2 && p.cross.x_uInfo == 'b' && p.cross.x_lInfo == 'b')
+        return 0;
+    if (p.cross.y == 3 && p.cross.x_l == 1 && (p.cross.x_u == 1 || p.cross.x_u == 2) &&
+            (p.cross.x_uInfo == 'b' || p.cross.x_uInfo == 'r') && p.cross.x_lInfo == 'l')
+        return 2;
+    if (p.cross.y >= 3 && p.cross.x_l == 1 && p.cross.x_u == 1 && p.cross.x_uInfo == 'r' && p.cross.x_lInfo == 'r')
+        return 3;
+    if (p.cross.y == 1 && p.cross.x_l == 1 && p.cross.x_u == 2 && p.cross.x_uInfo == 'b' && p.cross.x_lInfo == 'r')
+        return 4;
+    if (p.cross.y == 3 && p.cross.x_l == 1 && p.cross.x_u == 1 && p.cross.x_uInfo == 'l' && p.cross.x_lInfo == 'r')
+        return 5;
+    if (p.cross.y == 3 && p.cross.x_l == 2 && p.cross.x_u == 1 && p.cross.x_uInfo == 'l' && p.cross.x_lInfo == 'b')
+        return 6;
+    if (p.cross.y == 2 && p.cross.x_l == 1 && p.cross.x_u == 1 && p.cross.x_uInfo == 'r' && (p.cross.x_lInfo == 'r' || p.cross.x_lInfo == 'l'))
+        return 7;
+    if (p.cross.y == 3 && p.cross.x_l == 2 && p.cross.x_u == 2 && p.cross.x_uInfo == 'b' && p.cross.x_lInfo == 'b')
+        return 8;
+    if (p.cross.y == 3 && p.cross.x_l == 1 && p.cross.x_u == 2 && p.cross.x_uInfo == 'b' && p.cross.x_lInfo == 'r')
+        return 9;
+    if (p.cross.y == 1 && (p.cross.x_u == 1 || p.cross.x_u == 2) && p.cross.x_l == 1) // ? i think else statement w'd be better solution
+        return 1;
+
+    return -1;
+}
+
 void recognizeNumber(bool arr[COLS_MAX_SIZE][ROWS_MAX_SIZE])
 {
-    getAttr(arr);
-    cout << endl << endl;
+    ::Point point = getAttr(arr);
+    point = getCross(arr,point);
+    point.info();
+    int result = getResult(arr,point);
+    if (result != -1)
+    printf("\n----------------------------------\n"
+           "Result:\t%d",result);
+    else printf("\nWtf? Can't recognize the symbol o_O\n");
 }
 
 int main()
@@ -160,7 +284,7 @@ int main()
         }
     }
     //cout << "SOURCE = " << endl << " " << M << endl << endl;
-    cout << "\n-----------------------------------------------------------\n";
+    std::cout << "\n-----------------------------------------------------------\n";
     drawArr(arr);
     recognizeNumber(arr);
 
